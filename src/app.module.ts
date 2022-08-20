781e9bcfb35bd09e2fe1912d4a729922f40427d2
Viktor Sivulskiy <info@softmaker.kz>
2022-08-20 09:02:25 +0600

    Typeorm was added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "postgres",
+      database: "nest-js-news",
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
 })
 export class AppModule {}
